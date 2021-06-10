07b5cc144e09c056a0b00bc4362715bc98ba6a31
iriabov5 <iriabov5@yandex.ru>
2021-06-10 07:23:09 +0300

    Spring security JDBC auth commit 2

--- src/main/java/ru/geekbrains/springsecurity/configs/SecurityConfig.java
+++ src/main/java/ru/geekbrains/springsecurity/configs/SecurityConfig.java
@@ -1,19 +1,24 @@
 package ru.geekbrains.springsecurity.configs;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.provisioning.JdbcUserDetailsManager;
-
-import javax.sql.DataSource;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import ru.geekbrains.springsecurity.services.UserService;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    private UserService userService;
+
+    @Autowired
+    public void setUserService(UserService userService) {
+        this.userService = userService;
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
@@ -48,27 +53,42 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     // Spring security JDBC auth
 
-    @Bean
-    public JdbcUserDetailsManager users(DataSource dataSource) {
-//        UserDetails user = User.builder()
-//                .username("user")
-//                .password("{bcrypt}$2y$12$p063E5reS4B76546lqKGNeFeHWsULH5tTF.BfYy3fXMumRCxKXZvC")
-//                .roles("USER")
-//                .build();
+//    @Bean
+//    public JdbcUserDetailsManager users(DataSource dataSource) {
+////        UserDetails user = User.builder()
+////                .username("user")
+////                .password("{bcrypt}$2y$12$p063E5reS4B76546lqKGNeFeHWsULH5tTF.BfYy3fXMumRCxKXZvC")
+////                .roles("USER")
+////                .build();
+////
+////        UserDetails admin = User.builder()
+////                .username("admin")
+////                .password("{bcrypt}$2y$12$p063E5reS4B76546lqKGNeFeHWsULH5tTF.BfYy3fXMumRCxKXZvC")
+////                .roles("ADMIN", "USER")
+////                .build();
 //
-//        UserDetails admin = User.builder()
-//                .username("admin")
-//                .password("{bcrypt}$2y$12$p063E5reS4B76546lqKGNeFeHWsULH5tTF.BfYy3fXMumRCxKXZvC")
-//                .roles("ADMIN", "USER")
-//                .build();
+//        //        if (!users.userExists(user.getUsername())) {
+////            users.createUser(user);
+////        }
+////        if (!users.userExists(admin.getUsername())) {
+////            users.createUser(admin);
+////        }
+//        return new JdbcUserDetailsManager(dataSource);
+//    }
 
-        //        if (!users.userExists(user.getUsername())) {
-//            users.createUser(user);
-//        }
-//        if (!users.userExists(admin.getUsername())) {
-//            users.createUser(admin);
-//        }
-        return new JdbcUserDetailsManager(dataSource);
+    // Spring security DAO authentication provider
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public DaoAuthenticationProvider daoAuthenticationProvider() {
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setPasswordEncoder(passwordEncoder());
+        authenticationProvider.setUserDetailsService(userService);
+        return authenticationProvider;
     }
 
 }
--- src/main/java/ru/geekbrains/springsecurity/controllers/MainController.java
+++ src/main/java/ru/geekbrains/springsecurity/controllers/MainController.java
@@ -1,20 +1,34 @@
 package ru.geekbrains.springsecurity.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ru.geekbrains.springsecurity.entities.User;
+import ru.geekbrains.springsecurity.services.UserService;
 
 import java.security.Principal;
 
 @RestController
 public class MainController {
+    private UserService userService;
+
+    @Autowired
+    public void setUserService(UserService userService) {
+        this.userService = userService;
+    }
+
     @GetMapping("/")
     public String homePage() {
         return "home";
     }
 
     @GetMapping("/authenticated")
-    public String pageForAuthenticatedUsers(Principal principal) {
-        return "Secure part of web service: " + principal.getName();
+    public String pageForAuthenticatedUsers(Principal principal, Model model) {
+         User user = userService.findByUsername(principal.getName());
+         model.addAttribute(user);
+        return "Secure part of web service: " + user.getUsername() + ", " + user.getEmail();
+
     }
 
     @GetMapping("/read_profile")
